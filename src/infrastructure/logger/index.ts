fbf0bb4f08ed32f0eecb5125aa8e5f4b7a2abe78
Gabriel Juchault <gabriel.juchault@gmail.com>
2023-03-24 15:09:13 +0100

    feat: add infrastructure calls

---
+++ src/infrastructure/logger/index.ts
@@ -0,0 +1,52 @@
+import { pino, Logger as PinoLogger } from "pino";
+import { pinoMixin as telemetryMixin } from "../telemetry/instrumentations/pino.js";
+
+export type Logger = PinoLogger;
+export type LogLevel = "fatal" | "error" | "warn" | "info" | "debug" | "trace";
+
+export function createMockLogger(): Logger {
+  return createLogger("mock-logger", {
+    logLevel: "error",
+  });
+}
+
+export function createLogger(
+  serviceName: string,
+  config: { logLevel: LogLevel }
+): Logger {
+  const logger = pino({
+    name: "app",
+    level: config.logLevel,
+    formatters: {
+      // format level as string instead of number
+      level(label) {
+        return { level: label };
+      },
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
+    hooks: {
+      // reverse pino method so it goes logger.method(message, details) instead
+      // of logger.method(details, message)
+      logMethod(inputArguments: unknown[], method) {
+        if (inputArguments.length >= 2) {
+          const argument1 = inputArguments[0];
+          const argument2 = inputArguments[1];
+          Reflect.apply(method, this, [
+            argument2,
+            argument1,
+            ...inputArguments.slice(2),
+          ]);
+
+          return;
+        }
+
+        method.apply(this, inputArguments as [string, ...unknown[]]);
+      },
+    },
+    mixin: telemetryMixin,
+  });
+
+  return logger.child({
+    serviceName,
+  });
+}
---
+++ src/infrastructure/telemetry/instrumentations/slonik.ts
@@ -0,0 +1,99 @@
+import { SpanKind, Span } from "@opentelemetry/api";
+import {
+  DbSystemValues,
+  SemanticAttributes,
+} from "@opentelemetry/semantic-conventions";
+import { Interceptor, QueryContext } from "slonik";
+import type { Telemetry } from "../index.js";
+
+export const PG_VALUES = "db.postgresql.values";
+export const IDLE_TIMEOUT_MILLIS = "db.postgresql.idle.timeout.millis";
+export const MAX_CLIENT = "db.postgresql.max.client";
+
+export function getSpanOptions({
+  idleTimeout,
+  maximumPoolSize,
+  config,
+}: {
+  readonly config: { databaseUrl: string };
+  readonly idleTimeout: number;
+  readonly maximumPoolSize: number;
+}) {
+  return {
+    kind: SpanKind.CLIENT,
+    attributes: {
+      ...getCommonSpanOptions(config),
+      ["db.postgresql.idle.timeout.millis"]: idleTimeout,
+      ["db.postgresql.max.client"]: maximumPoolSize,
+    },
+  };
+}
+
+export function createSlonikTelemetryInterceptor({
+  telemetry,
+  config,
+}: {
+  telemetry: Telemetry;
+  readonly config: { databaseUrl: string };
+}): Interceptor {
+  const spanByQueryId = new Map<string, Span>();
+
+  return {
+    beforeQueryExecution(queryContext, query) {
+      const span = telemetry.tracer.startSpan("database.query", {
+        kind: SpanKind.CLIENT,
+        attributes: {
+          ...getCommonSpanOptions(config),
+          [SemanticAttributes.DB_OPERATION]: getQueryOperationName(query.sql),
+          [SemanticAttributes.DB_STATEMENT]: query.sql,
+          [PG_VALUES]: query.values.toString(),
+        },
+      });
+
+      spanByQueryId.set(getQueryId(queryContext), span);
+
+      return null;
+    },
+    afterQueryExecution(queryContext) {
+      const span = spanByQueryId.get(getQueryId(queryContext));
+
+      if (!span) {
+        return null;
+      }
+
+      span.end();
+
+      return null;
+    },
+  };
+}
+
+function getCommonSpanOptions(config: { databaseUrl: string }) {
+  const databaseUrl = new URL(config.databaseUrl);
+  databaseUrl.password = "";
+
+  return {
+    [SemanticAttributes.DB_SYSTEM]: DbSystemValues.POSTGRESQL,
+    [SemanticAttributes.DB_NAME]: databaseUrl.pathname.slice(1),
+    [SemanticAttributes.NET_PEER_NAME]: databaseUrl.hostname,
+    [SemanticAttributes.NET_PEER_PORT]: databaseUrl.port,
+    [SemanticAttributes.DB_CONNECTION_STRING]: databaseUrl.toString(),
+    [SemanticAttributes.DB_USER]: databaseUrl.username,
+  };
+}
+
+function getQueryId(queryContext: QueryContext): string {
+  return [
+    queryContext.connectionId,
+    queryContext.poolId,
+    queryContext.queryId,
+  ].join("-");
+}
+
+function getQueryOperationName(sql: string): string {
+  const words = sql
+    .trim()
+    .split(" ")
+    .filter((word) => word.length > 0);
+  return words.length > 0 ? words[0].toLowerCase() : "unknown";
+}
