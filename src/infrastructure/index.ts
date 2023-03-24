c5cc90323cb38216b0b3aafe693414c64c45a48f
Gabriel Juchault <gabriel.juchault@gmail.com>
2023-03-24 16:37:18 +0100

    feat(infrastructure): add shutdown manager

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -5,3 +5,4 @@ export * from "./http/index.js";
 export * from "./logger/index.js";
 export * from "./task-scheduling/index.js";
 export * from "./telemetry/index.js";
+export * from "./shutdown/index.js";
