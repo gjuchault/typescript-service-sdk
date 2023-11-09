c2eabe55921095fb769f42782e069edfe2b82ce1
Gabriel Juchault <gabriel.juchault@gmail.com>
2023-11-09 16:57:30 +0100

    fix: format

--- src/slonik/__tests__/index.test.ts
+++ src/slonik/__tests__/index.test.ts
@@ -47,7 +47,7 @@ describe("createMockDatabase()", () => {
 
       before(async () => {
         result = await database.any(
-          sql.type(z.object({ id: z.number() }))`select id from foobar`
+          sql.type(z.object({ id: z.number() }))`select id from foobar`,
         );
       });
 
@@ -73,7 +73,7 @@ describe("prepareBulkInsert()", () => {
           ["index", "int4"],
         ],
         [{ id: "some-uuid", my_name: "John Doe", index: 1 }],
-        (user) => ({ id: user.id, name: user.my_name, index: user.index })
+        (user) => ({ id: user.id, name: user.my_name, index: user.index }),
       );
 
       it("returns the columns and data grid", () => {
