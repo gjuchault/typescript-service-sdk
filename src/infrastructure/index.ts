a5cf643fa9e5f09a97a5437cbea134a166a6e7b4
Gabriel Juchault <gabriel.juchault@gmail.com>
2023-11-21 17:00:36 +0100

    fix(exports): export factories top level

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,7 +1,7 @@
 export type { Cache } from "./cache/index.js";
-export * as cache from "./cache/index.js";
+export { createCacheStorage } from "./cache/index.js";
 export type { Database } from "./database/index.js";
-export * as database from "./database/index.js";
+export { createDatabase } from "./database/index.js";
 export * as databaseMigration from "./database/migration.js";
 export type {
   HttpReply,
@@ -9,12 +9,12 @@ export type {
   HttpServer,
   RouterImplementation,
 } from "./http/index.js";
-export * as http from "./http/index.js";
+export { createHttpServer } from "./http/index.js";
 export type { Logger, LogLevel } from "./logger/index.js";
-export * as logger from "./logger/index.js";
+export { createLogger, createMockLogger } from "./logger/index.js";
 export type { ShutdownManager } from "./shutdown/index.js";
 export * as shutdown from "./shutdown/index.js";
 export type { TaskScheduling } from "./task-scheduling/index.js";
-export * as taskScheduling from "./task-scheduling/index.js";
+export { createTaskScheduling } from "./task-scheduling/index.js";
 export type { Telemetry } from "./telemetry/index.js";
-export * as telemetry from "./telemetry/index.js";
+export { createTelemetry } from "./telemetry/index.js";
